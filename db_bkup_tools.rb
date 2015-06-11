a23cb0595dc63ddebfbef6110269e193f600e2fa
Ramsey <rdarling42@gmail.com>
2015-06-11 16:17:33 -0400

    poop

--- db_bkup_tools.rb
+++ db_bkup_tools.rb
@@ -20,14 +20,20 @@ def db_bkup_tools_main
 end
 
 def dbbkupt_generate_remote_db_dump
-
+    #step one
+    #create tunnel
     gateway = Net::SSH::Gateway.new(@ssh_host,@ssh_user)
-    #port = gateway.open('127.0.0.1', 3306, 3307)
-    client = Mysql2::Client.new(
-        host: @db_host,
-        username: @db_user,
-        password: @db_pass,
-        database: @db_name,
-        port: port
-    )
+    
+end
+
+def dbbkupt_download_db_dump
+    #step two
+end
+
+def dbbkupt_replace_local_db_instance
+    #step three
+    client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass)
+    client.query("DROP DATABASE IF EXISTS #{@db_name}")
+    client.query("CREATE DATABASE #{@db_name}")
+    client.close
 end
