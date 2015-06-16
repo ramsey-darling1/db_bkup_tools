067ea9218601f921f760558e5b54df1ed6563579
Ramsey <rdarling42@gmail.com>
2015-06-16 09:51:58 -0400

    starting to build interface

--- db_bkup_tools.rb
+++ db_bkup_tools.rb
@@ -16,15 +16,43 @@ require 'net/ssh/gateway'
 
 
 def db_bkup_tools_main
-    dbbkupt_generate_remote_db_dump()
+    puts "Hello, welcome to db bkup tools"
+    puts "Enter help for help"
+    input = get_user_input()
+    manage_input(input)
 end
 
-def generate_remote_db_dump
+def manage_input(i)
+    case i
+    when i == 'help'
+        puts "You asked for help!"
+        puts "Available commands are:"
+
+    else
+        #recurse until we get something useful
+        puts "Sorry, request not understood"
+        input = get_user_input()
+        manage_input(input)
+    end
+end
+
+def get_user_input
+    puts "Please enter your command: "
+    return gets.strip
+end
+
+def generate_gateway
     gateway = Net::SSH::Gateway.new(@ssh_host,@ssh_user)
+    gateway.open('127.0.0.1', 27017, 27018)
+    return gateway
 end
 
-def generate_dump(file_name)
-    exec "mysqldump -p -u #{@db_user} -h localhost #{@db_name} > #{file_name}"
+def shutdown_gateway(gateway)
+    gateway.shutdown!
+end
+
+def generate_dump(user,name,file_name)
+    exec "mysqldump -p -u #{user} -h localhost #{name} > #{file_name}"
 end
 
 def download_db_dump(path_to_file)
@@ -35,9 +63,9 @@ def import_db(import_file)
     exec "mysql -p -u #{@db_user} -h localhost #{@db_name} < #{import_file}"
 end
 
-def replace_db_instance
-    client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass)
-    client.query("DROP DATABASE IF EXISTS #{@db_name}")
-    client.query("CREATE DATABASE #{@db_name}")
+def replace_db_instance(host,user,pass,name)
+    client = Mysql2::Client.new(:host => host, :username => user, :password => pass)
+    client.query("DROP DATABASE IF EXISTS #{name}")
+    client.query("CREATE DATABASE #{name}")
     client.close
 end
