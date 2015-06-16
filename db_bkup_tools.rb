87ea44bde102826d60a26df60371cab23c6ed85a
Ramsey <rdarling42@gmail.com>
2015-06-16 09:24:48 -0400

    working on it

--- db_bkup_tools.rb
+++ db_bkup_tools.rb
@@ -1,4 +1,5 @@
 #automating some tasks that I have to often do
+#creating and pulling down database dumps
 #@rdar
 
 require 'mysql2'
@@ -15,23 +16,26 @@ require 'net/ssh/gateway'
 
 
 def db_bkup_tools_main
-    #step one, ssh into remove server, do a db dump 
     dbbkupt_generate_remote_db_dump()
 end
 
-def dbbkupt_generate_remote_db_dump
-    #step one
-    #create tunnel
+def generate_remote_db_dump
     gateway = Net::SSH::Gateway.new(@ssh_host,@ssh_user)
-    
 end
 
-def dbbkupt_download_db_dump
-    #step two
+def generate_dump(file_name)
+    exec "mysqldump -p -u #{@db_user} -h localhost #{@db_name} > #{file_name}"
 end
 
-def dbbkupt_replace_local_db_instance
-    #step three
+def download_db_dump(path_to_file)
+    exec "scp #{@ssh_user}@#{@ssh_host}:#{path_to_file} ."
+end
+
+def import_db(import_file)
+    exec "mysql -p -u #{@db_user} -h localhost #{@db_name} < #{import_file}"
+end
+
+def replace_db_instance
     client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_pass)
     client.query("DROP DATABASE IF EXISTS #{@db_name}")
     client.query("CREATE DATABASE #{@db_name}")
